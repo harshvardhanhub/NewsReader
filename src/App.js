156448fac6e4db559a0e571092ee11d4be82fe7a
harshvardhanhub <harshvardhan.anand123@gmail.com>
2023-12-31 03:54:39 +0530

    Updated and had added the search tab for fillter.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ const App = () => {
   const [isLoggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = (name, email) => {
-    // Check if both name and email are provided
     if (name && email) {
-      // Add your authentication logic here
-      // For simplicity, let's assume any non-empty name and email are valid
       setLoggedIn(true);
     } else {
       alert('Please enter your name and email.');
--- src/components/news/News.js
+++ src/components/news/News.js
@@ -6,27 +6,44 @@ import './News.css';
 const News = () => {
   const [myNews, setMyNews] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const fetchData = async () => {
     try {
+      setLoading(true);
+      setError(null);
+
       const response = await fetch(
         'https://newsapi.org/v2/everything?q=bitcoin&apiKey=f817b67d1d9f4d33938ed9cc8e2fa23d'
       );
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch news: ${response.statusText}`);
+      }
+
       const data = await response.json();
       setMyNews(data.articles);
     } catch (error) {
-      console.error('Error fetching news:', error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [searchQuery]); 
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
     document.body.classList.toggle('dark-mode-on', !darkMode);
   };
+
+  const filteredNews = myNews.filter((article) =>
+    article.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div className={`news-container ${darkMode ? 'dark-mode-on' : ''}`}>
@@ -40,6 +57,19 @@ const News = () => {
           {darkMode ? 'Dark Mode' : 'Light Mode'}
         </span>
       </div>
+
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search for news..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      
       <Tabs>
         <TabList>
           <Tab>Page 1</Tab>
@@ -59,8 +89,12 @@ const News = () => {
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((pageNumber) => (
           <TabPanel key={pageNumber}>
             <div className="mainDiv">
-              {myNews.slice((pageNumber - 1) * 4, pageNumber * 4).map((article, index) => (
-                <div className="card" key={index}>
+              {filteredNews.length === 0 ? (
+                <p>No results found for '{searchQuery}'</p>
+              ) : (
+                filteredNews.slice((pageNumber - 1) * 4, pageNumber * 4).map((article, index) => (
+                  <div className="card" key={index}>
+
                   <img
                     src={article.urlToImage || 'https://via.placeholder.com/300'}
                     alt={article.title}
@@ -78,8 +112,9 @@ const News = () => {
                       Read More
                     </a>
                   </div>
-                </div>
-              ))}
+                 </div>
+                ))
+              )}
             </div>
           </TabPanel>
         ))}
